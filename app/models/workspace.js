2d2e041040ebfc1facd138efedb8301c6ba2c144
Christoph <christoph@ag-prop.com>
2020-05-21 13:51:23 +0200

    Feat(Workspace): Add better initials

--- app/models/workspace.js
+++ app/models/workspace.js
@@ -16,13 +16,22 @@ export default class WorkspaceModel extends Model {
   @hasMany('user') users;
   @hasMany('venue') venues;
 
+
   // Getter and setter
   get displayLabel() {
     return this.name;
   }
 
   get initials() {
-    let name = this.name || '??';
+    let name = this.name || '--';
+    let nameSegments = name.trim().split(' ');
+
+    if (nameSegments.length >= 2) {
+      let first = nameSegments[0];
+      let last = nameSegments[nameSegments.length - 1];
+
+      return `${first.substring(0, 1)}${last.substring(0, 1)}`;
+    }
 
     return `${name.substring(0, 2)}`;
   }
